4f35ff1d6e23748f404b84f702d88386f996ae5a
Anthony Willingham <anthonywillingham@Anthonys-MacBook-Pro.local>
2026-01-27 18:25:46 -0800

    Deploy website - based on f6d97cc188683303c1c2b55c6d8f32f7b9171a7b

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/anthonywillingham/greenshirt-university/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/anthonywillingham/greenshirt-university/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/anthonywillingham/greenshirt-university/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/anthonywillingham/greenshirt-university/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/anthonywillingham/greenshirt-university/src/css/custom.css"),
-];
